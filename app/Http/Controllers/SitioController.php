876cbec2dda936674a072878da9a2fa1a25fa671
Estudiante <estuam@UAM.LOCAL>
2017-02-25 08:53:21 -0500

    Primer commit

---
+++ app/Http/Controllers/SitioController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SitioController extends Controller
+{
+    public function index()
+    {
+        echo "Hola Mundo";
+    }
+
+    public function saludar(Request $request, $nombre)
+    {
+        echo "Hola" . $nombre;
+    }
+}
